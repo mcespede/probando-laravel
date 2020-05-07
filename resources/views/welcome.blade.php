55f48e7ad7358cf4d3f234c3992f7b40f1a74a0f
Mauro Céspedes <cespedes.mauro@gmail.com>
2020-05-07 13:14:11 +0200

    Segundo test  con el servidor  de applicaciones

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Mauro
+                    Mauro Cespedes Araya
                 </div>
 
                 <div class="links">
